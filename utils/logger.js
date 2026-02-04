2832a28c868d2f375279118d5429c905add2c888
microsoftedge <119023239+testbug116117118@users.noreply.github.com>
2026-02-04 21:56:04 +0530

    Create logger.js

---
+++ utils/logger.js
@@ -0,0 +1,30 @@
+const winston = require('winston');
+const config = require('../config/config');
+
+const logger = winston.createLogger({
+  level: config.logger.level,
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    })
+  ]
+});
+
+// If we're not in production, log to the console with pretty colors
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    )
+  }));
+}
+
+module.exports = logger;
